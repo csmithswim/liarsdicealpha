49113eb2e9c7498f19cc3b158bf009ac12975943
Cullen S <cullensmi@gmail.com>
2021-01-12 17:43:16 -0500

    Updated main class

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,14 +1,9 @@
 package com.csmithswim;
 
 
+
 public class Main {
     public static void main(String[] args) {
-        Player player = new Player("player1");
-        player.roll();
-        player.peek();
-        int[] claim = player.getClaim();
-        System.out.println("Player claimed there are " + claim[1] + " " + claim[0] + "s");
-        boolean decision = player.getDecision();
-        System.out.println(decision ? "Lie called" : "Play on");
+        LiarsDice game = new LiarsDice(2);
     }
 }
