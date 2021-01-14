13ad804bbebf91bc4b7af24a75566583817f786e
Cullen S <cullensmi@gmail.com>
2021-01-14 18:53:11 -0500

    Added comments to main class

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,9 +1,18 @@
 package com.csmithswim;
 
-
-
 public class Main {
     public static void main(String[] args) {
-        LiarsDice game = new LiarsDice(2);
+        LiarsDice game = new LiarsDice(3);
+
+        // improvements:
+        // Print out end of round info. Call out liar and declare who lost a die.
+        // declare when a player is removed from game.
+        // improve overall print outs.
+        // clear console at start of next player turn.
+        // allow user to enter how many players.
+
+        // Major improvement:
+        // each round should start with a new player.
+        // clean code.
     }
 }
